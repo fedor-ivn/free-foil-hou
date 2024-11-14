96ddbd6fe0c3f913a54cd5f3790c59d165e20f9f
Damir Afliatonov <afl_damir@mail.ru>
2024-11-14 02:19:28 +0000

    Fix flexrigid case

--- src/Language/Lambda/FCU/Unification.hs
+++ src/Language/Lambda/FCU/Unification.hs
@@ -75,8 +75,9 @@ abst (x : xs, t) = x :.: abst (xs, t)
 -- λz1 . (λz2 . (z w))
 
 hnf :: ([Id], [Char], [Id]) -> Term
-hnf (vars, base, args) = 
-  foldr (\x acc -> x :.: acc)
+hnf (vars, base, args) =
+  foldr
+    (\x acc -> x :.: acc)
     (foldl (\acc x -> acc :@ O x) (Constructor base) args)
     vars
 
@@ -98,7 +99,7 @@ discharge th t = case lookup t th of
 -- z
 
 -- >>> discharge [("Fst" :@ "x", "z")] ("Cons" :@ ("Fst" :@ "x") :@ "w")
--- (Cons z) w
+-- (Cons (z)) (w)
 
 foldlN :: (([(Id, Term)], Term) -> [(Id, Term)]) -> ([(Id, Term)], [Term]) -> [(Id, Term)]
 foldlN _ (rho, []) = rho
@@ -114,9 +115,17 @@ subset sm tn = all (`elem` tn) sm
 -- False
 
 eqsel :: [Id] -> [Term] -> [Term] -> [Id]
-eqsel vsm tn sm =
-  let indices = [i | (i, (t1, t2)) <- zip [0 ..] (zip tn sm), t1 /= t2]
-   in [v | (i, v) <- zip [0 ..] vsm, i `elem` indices]
+eqsel vsm tn sm = 
+    [v | (v, t) <- zip vsm tn, not (t `elem` sm)]
+  
+-- >>> eqsel ["z1", "z2"] ["x", "y", "z"] ["x", "y", "z"]  
+-- []
+
+-- >>> eqsel ["z1", "z2"] ["x", "y"] ["x", "w"]
+-- ["z2"]
+
+-- >>> eqsel ["z1", "z2"] ["x", "y"] ["z", "x"]
+-- ["z2"]
 
 prune :: [Term] -> ([(Id, Term)], Term) -> [(Id, Term)]
 prune tn (rho, u) = case strip (devar rho u) of
@@ -134,7 +143,6 @@ prune tn (rho, u) = case strip (devar rho u) of
         let vsm = mkvars sm
          in (_W, hnf (vsm, _W ++ "'", eqsel vsm tn sm)) : rho
 
-
 -- >>> prune ["x", "y"] ([], "Cons" :@ "x" :@ "y")
 -- []
 
@@ -147,20 +155,26 @@ prune tn (rho, u) = case strip (devar rho u) of
 ------- Unification ----- bvs (th (s,t)) = Q, (theta, S)
 unify :: [(Char, Id)] -> ([(Id, Term)], (Term, Term)) -> [(Id, Term)]
 unify bvs (th, (s, t)) = case ((devar th s), (devar th t)) of
-  (x :.: s, x' :.: t) -> unify (('B', x) : bvs) (th, (s, if x == x' then t else rename x x' t))
+  (x :.: s, x' :.: t) ->
+    let renamed = if x == x' then t else rename x' x t
+        newBvs = ('B', x) : bvs
+     in unify newBvs (th, (s, renamed))
   (s, t) -> cases bvs (th, (s, t))
 
 -- >>> unify [] ([], ("x", "x"))
 -- []
 
 -- >>> unify [] ([], ("x", "y"))
--- Different terms, not unifiable
+-- x and y are not unifiable
+
+-- >>> unify [] ([], ("l1" :.: ("x" :.: "l1"), "l2" :.: ("x" :.: "l2")))
+-- []
 
 -- >>> unify [] ([], ("Cons" :@ "y" :@ "z", "Cons" :@ "y" :@ "z"))
 -- []
 
 -- >>> unify [] ([], ("Cons" :@ "y" :@ "z", "Cons" :@ "y" :@ "w"))
--- Different terms, not unifiable
+-- z and w are not unifiable
 
 -- >>> unify [] ([], ("X" :@ "y", "Cons" :@ "x"))
 -- [("X",λz1 . (Cons x))]
@@ -171,35 +185,43 @@ unify bvs (th, (s, t)) = case ((devar th s), (devar th t)) of
 -- >>> unify [] ([], ("X" :@ "y" :@ "x", "Cons" :@ "x" :@ ("Cons" :@ "z" :@ "x")))
 -- [("X",λz1 . (λz2 . ((Cons z2) (Cons z) z2)))]
 
+-- Example from the paper
+-- >>> unify [] ([], ("l1" :.: ("l2" :.: ("X" :@ ("Fst" :@ "l1") :@ ("Fst" :@ ("Snd" :@ "l2")))), "l1" :.: ("l2" :.: ("Snd" :@ (("Y" :@ ("Fst" :@ "l2")) :@ ("Fst" :@ "l1"))))))
+-- [("Y",λz1 . (λz2 . (Y' z2))),("X",λz1 . (λz2 . (Snd ((λz1 . (λz2 . (Y' z2))) (Fst l2)) (z1))))]
+
+
 cases :: [(Char, Id)] -> ([(Id, Term)], (Term, Term)) -> [(Id, Term)]
 cases bvs (th, (s, t)) = case (strip s, strip t) of
-  ((W _F, sn), (W _G, zm)) -> caseFlexFlex bvs (_F,sn,_G,zm,th)
+  ((W _F, sn), (W _G, zm)) -> caseFlexFlex bvs (_F, sn, _G, zm, th)
   ((W _F, sn), (Constructor _, _)) -> caseFlexRigid bvs (_F, sn, t, th)
   ((Constructor _, _), (W _F, sn)) -> caseFlexRigid bvs (_F, sn, s, th)
-  ((a, []), (b, [])) -> if a == b then th else error "Different terms, not unifiable"
+  ((a, []), (b, [])) -> if a == b then th else error (show a ++ " and " ++ show b ++ " are not unifiable")
   ((a, sn), (b, tm)) -> caseRigidRigid bvs (a, sn, b, tm, th)
 
 caseFlexRigid :: [(Char, Id)] -> (Id, [Term], Term, [(Id, Term)]) -> [(Id, Term)]
 caseFlexRigid bvs (_F, tn, s, rho) -- s is rigid
   | occ _F rho s = error "Restriction fail at flexrigid case"
-  | otherwise =
+  | otherwise = 
       let zn = mkvars tn
-          discharged = discharge (zip tn zn) s
+          pruningResult = prune tn (rho, s)
+          pruned = devar pruningResult s
+          discharged = discharge (zip tn zn) pruned
           theta = (_F, abst (zn, discharged))
-       in prune tn (theta : rho, discharged)
+          substitutions = pruningResult ++ [theta] ++ rho
+      in substitutions
+
 
 caseRigidRigid :: [(Char, Id)] -> (Term, [Term], Term, [Term], [(Id, Term)]) -> [(Id, Term)]
 caseRigidRigid bvs (a, sn, b, tm, th) = case (a, b) of
   (O x, O y) -> applicableCase bvs (x, y, sn, tm, th)
   (Constructor x, Constructor y) -> applicableCase bvs (x, y, sn, tm, th)
-  otherwise -> error "Unexpected case in (2) rule"
+  otherwise -> error (show a ++ " and " ++ show b ++ " are not unifiable")
   where
     applicableCase :: [(Char, Id)] -> (Id, Id, [Term], [Term], [(Id, Term)]) -> [(Id, Term)]
     applicableCase bvs (x, y, sn, tm, th) =
       if (x == y && length sn == length tm)
         then foldl (\th' (s, t) -> unify bvs (th', (s, t))) th (zip sn tm)
         else error "Different function heads or argument lists lengths in (2) rule"
-
 
 caseFlexFlex :: [(Char, Id)] -> (Id, [Term], Id, [Term], [(Id, Term)]) -> [(Id, Term)]
 caseFlexFlex bvs (_F, sn, _G, tm, th) = error "FlexFlex case not implemented"
