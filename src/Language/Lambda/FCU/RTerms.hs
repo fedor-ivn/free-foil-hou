0e939eeac2169ce53461d00c529bddf6970a3a9e
Damir Afliatonov <afl_damir@mail.ru>
2024-12-23 04:25:28 +0000

    Format and add comments

--- src/Language/Lambda/FCU/RTerms.hs
+++ src/Language/Lambda/FCU/RTerms.hs
@@ -7,26 +7,6 @@ import Language.Lambda.FCU.Terms (Id, Term (..))
 data RTerm = RO Id | RConstructor Id | RApp RTerm RTerm
   deriving (Eq, Show)
 
-toRTerm :: Term -> RTerm
-toRTerm (O x) = RO x
-toRTerm (Constructor x :@ y) = RApp (RConstructor x) (toRTerm y)
-toRTerm (x :@ y) = RApp (toRTerm x) (toRTerm y)
-toRTerm (W _) = error "Metavars are not allowed in Restricted Terms"
-toRTerm (Constructor _) = error "Constructor terms should have > 0 arguments"
-toRTerm (_ :.: _) = error "Abstraction is not allowed in Restricted Terms"
-
-isRTerm :: Term -> Bool
-isRTerm (O _) = True
-isRTerm (Constructor _ :@ y) = isRTerm y
-isRTerm (x :@ y) = isRTerm x && isRTerm y
-isRTerm _ = False
-
-
-toTerm :: RTerm -> Term
-toTerm (RO x) = O x
-toTerm (RConstructor x) = Constructor x
-toTerm (RApp f b) = toTerm f :@ toTerm b
-
 -- >>> toRTerm ("Cons" :@ "x" :@ "y")
 -- RApp (RApp (RConstructor "Cons") (RO "x")) (RO "y")
 
@@ -42,5 +22,39 @@ toTerm (RApp f b) = toTerm f :@ toTerm b
 -- >>> toRTerm ("Cons" :@ "x" :@ "Fst")
 -- Constructor terms should have > 0 arguments
 
+-- | Convert a Term to a Restricted Term
+toRTerm :: Term -> RTerm
+toRTerm (O x) = RO x
+toRTerm (Constructor x :@ y) = RApp (RConstructor x) (toRTerm y)
+toRTerm (x :@ y) = RApp (toRTerm x) (toRTerm y)
+toRTerm (W _) = error "Metavars are not allowed in Restricted Terms"
+toRTerm (Constructor _) = error "Constructor terms should have > 0 arguments"
+toRTerm (_ :.: _) = error "Abstraction is not allowed in Restricted Terms"
+
+-- >>> isRTerm ("Cons" :@ "x" :@ "y")
+-- True
+
+-- >>> isRTerm "X"
+-- False
+
+-- >>> isRTerm ("Cons" :@ "x" :@ "y" :@ "X")
+-- False
+
+-- >>> isRTerm ("l1" :.: "x" :@ "y")
+-- False
+
+-- | Check if a Term is a Restricted Term
+isRTerm :: Term -> Bool
+isRTerm (O _) = True
+isRTerm (Constructor _ :@ y) = isRTerm y
+isRTerm (x :@ y) = isRTerm x && isRTerm y
+isRTerm _ = False
+
 -- >>> toTerm (RApp (RApp (RConstructor "Cons") (RO "x")) (RO "y"))
 -- Cons (x) (y)
+
+-- | Convert a Restricted Term to a Term
+toTerm :: RTerm -> Term
+toTerm (RO x) = O x
+toTerm (RConstructor x) = Constructor x
+toTerm (RApp f b) = toTerm f :@ toTerm b
