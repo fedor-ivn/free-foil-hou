17e6b8d552ac52c4050d57ae3e689cc6d0809771
Damir Afliatonov <afl_damir@mail.ru>
2025-01-16 14:25:46 +0000

    Syntax draft

--- src/Language/Lambda/FCU/Parse.hs
+++ src/Language/Lambda/FCU/Parse.hs
@@ -1,18 +1,42 @@
-import qualified AbsSyntax as A
-import qualified ParSyntax as P
-import qualified LexSyntax as L
+module Language.Lambda.FCU.Parse where
+
+import qualified Language.Lambda.FCU.FCUSyntax.Abs as Raw
+import qualified Language.Lambda.FCU.FCUSyntax.Par as Raw
+import qualified Language.Lambda.FCU.FCUSyntax.Lex as Raw
+import qualified Language.Lambda.FCU.FCUSyntax.Layout as Raw
+import qualified Language.Lambda.FCU.FCUSyntax.Print as Raw
+import qualified Language.Lambda.FCU.FCUSyntax.ErrM as Raw
+import Language.Lambda.FCU.Terms
 
 parseTerm :: String -> Either String Term
-parseTerm input = case P.pTerm (L.tokens input) of
-  Left err -> Left $ "Parse error: " ++ show err
-  Right term -> Right $ convertTerm term
+parseTerm input = case Raw.pTerm . Raw.myLexer $ input of
+    Raw.Bad err -> Left $ "Parse error: " ++ err
+    Raw.Ok term -> Right $ convertTerm term
 
--- Convert BNFC AST to your Term type
-convertTerm :: A.Term -> Term
-convertTerm (A.Lam x t) = x :.: convertTerm t
-convertTerm (A.App t1 t2) = convertTerm t1 :@ convertTerm t2
-convertTerm (A.Var x) = O x
-convertTerm (A.MetaVar x) = W x
-convertTerm (A.Cons x) = Constructor x
+convertTerm :: Raw.Term -> Term 
+convertTerm term = case term of
+    Raw.Lam (Raw.Id x) t -> x :.: convertTerm t
+    Raw.App t1 t2 -> convertTerm t1 :@ convertTerm t2
+    Raw.Var (Raw.VarIdent x) -> O x
+    Raw.MetaVar (Raw.MetaVarIdent x) -> W x
 
--- >>> parseTerm "x"
+main :: IO ()
+main = do
+    let input = "λx. x y"
+    case parseTerm input of
+        Left err -> putStrLn err
+        Right term -> print term
+
+--- >>> parseTerm "x"
+-- /workspaces/haskell/lambda-free-foil/src/Language/Lambda/FCU/FCUSyntax/Par.hs:351:19: error: [GHC-88464]
+--     • Data constructor not in scope:
+--         Language.Lambda.FCUSyntax.Abs.Var :: VarIdent -> Term
+--       NB: no module named ‘Language.Lambda.FCUSyntax.Abs’ is imported.
+--     • In the first argument of ‘HappyAbsSyn15’, namely
+--         ‘(Language.Lambda.FCUSyntax.Abs.Var happy_var_1)’
+--       In the expression:
+--         HappyAbsSyn15 (Language.Lambda.FCUSyntax.Abs.Var happy_var_1)
+--       In an equation for ‘happyReduction_17’:
+--           happyReduction_17 (HappyAbsSyn9 happy_var_1)
+--             = HappyAbsSyn15 (Language.Lambda.FCUSyntax.Abs.Var happy_var_1)
+-- (deferred type error)
